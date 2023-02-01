b0c876303f778d9bd35d822c093f7e943087a305
ericmli <ericmli1334@gmail.com>
2023-02-01 16:46:06 -0300

    AttComAleatorio

--- AtividadeDOM/index.js
+++ AtividadeDOM/index.js
@@ -1,15 +1,28 @@
-function confirmar(){
+function confirmar() {
 
     let numero = document.getElementById('numero').value
 
-    
-    document.getElementById("listaTab").innerHTML = ''
-    for(let i = 0; i<= 10 ; i++){
 
-        document.getElementById("listaTab").innerHTML += '<li>'+ numero + ' X ' + i + ' = ' + (i * numero) +'</li>'
+    document.getElementById("listaTab").innerHTML = ''
+    for (let i = 0; i <= 10; i++) {
+
+        document.getElementById("listaTab").innerHTML += '<li>' + numero + ' X ' + i + ' = ' + (i * numero) + '</li>'
 
         console.log(i)
     }
-    numero = document.getElementById('numero').value =''
+    numero = document.getElementById('numero').value = ''
 
+}
+
+function aleatorio() {
+    document.getElementById("listaTab").innerHTML = ''
+    for (let i = 1; i <= 10; i++) {
+
+        for (let j = 0; j <= 10; j++) {
+
+            document.getElementById("listaTab").innerHTML += '<li>' + i + ' X ' + j + ' = ' + (i * j) + '</li>'
+
+            console.log(i)
+        }
+    }
 }
